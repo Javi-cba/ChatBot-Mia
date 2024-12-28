b4d96a878826be483584f4052be6b90ae844803f
Javi-cba <cordobajava@gmail.com>
2024-12-28 16:17:59 -0300

    JC

---
+++ frontend/src/services/services.chatbot.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const URL = import.meta.env.VITE_APP_URL;
+
+export const getChat = async (prompt) => {
+  try {
+    const response = await axios.post(`${URL}/chat/prompt-text`, null, {
+      params: { prompt },
+    });
+    return response.data.response;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
