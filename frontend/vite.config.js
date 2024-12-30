2d73be451c924b23704079f850aaf3f1c6589325
Javi-cba <cordobajava@gmail.com>
2024-12-30 13:30:25 -0300

    JC

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+const PORT = process.env.PORT || 5173;
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: PORT,
+    host: '0.0.0.0',
+  },
+});
